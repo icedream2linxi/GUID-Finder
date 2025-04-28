41cd531db953a399f2957826c1b025749b4bf6fd
linxi <linxi@rzon.tech>
2025-04-28 15:57:59 +0800

    使支持vs2022

--- Source/DumpLib/DumpLib.cpp
+++ Source/DumpLib/DumpLib.cpp
@@ -2,165 +2,152 @@
 #include <atlcom.h>
 #include <atlcomcli.h>
 #include <comdef.h>
-#include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <map>
-#include <boost/algorithm/string.hpp>
 
 using namespace std;
 using namespace ATL;
-using namespace std::tr2;
+namespace fs = std::filesystem;
 
-#define INTERFACE_FILENAME  "NewInterfaces.txt" 
-#define CLASS_FILENAME      "NewClasses.txt" 
+#define INTERFACE_FILENAME "NewInterfaces.txt"
+#define CLASS_FILENAME "NewClasses.txt"
 
-#define VERIFHR(hrfn) \
-if (FAILED((hr = hrfn))) _com_issue_error(hr)
+#define VERIFHR(hrfn)        \
+    if (FAILED((hr = hrfn))) \
+    _com_issue_error(hr)
 
-void DumpFile(map<wstring, pair<bool, wstring>> &guids, const sys::path &filePath)
+inline std::wstring to_lower_copy(const std::wstring& str)
 {
-	wcout << L"Dump " << filePath << endl;
-
-	HRESULT hr = S_OK;
-	ITypeLibPtr spTypeLib;
-	VERIFHR(LoadTypeLibEx(filePath.wstring().c_str(), REGKIND_NONE, &spTypeLib));
-
-	UINT uiTypeInfoCount = spTypeLib->GetTypeInfoCount();
-	for (UINT i = 0; i < uiTypeInfoCount; ++i)
-	{
-		TYPEKIND typeKind;
-		VERIFHR(spTypeLib->GetTypeInfoType(i, &typeKind));
-		if (typeKind != TKIND_COCLASS && typeKind != TKIND_DISPATCH)
-			continue;
-
-		ITypeInfoPtr spTypeInfo;
-		VERIFHR(spTypeLib->GetTypeInfo(i, &spTypeInfo));
-
-		_bstr_t bstrName;
-		VERIFHR(spTypeInfo->GetDocumentation(MEMBERID_NIL, bstrName.GetAddress(), NULL, NULL, NULL));
-
-		TYPEATTR *pTypeAttr = nullptr;
-		VERIFHR(spTypeInfo->GetTypeAttr(&pTypeAttr));
-
-		wchar_t buffer[256];
-		if (StringFromGUID2(pTypeAttr->guid, buffer, 256) == 0)
-			continue;
-
-		wstring strGuid = (buffer + 1);
-		strGuid.pop_back();
-
-		spTypeInfo->ReleaseTypeAttr(pTypeAttr);
-		pTypeAttr = nullptr;
-
-		auto iter = guids.find(strGuid);
-		if (iter != guids.end())
-			continue;
-
-		guids.insert(make_pair(strGuid, make_pair(typeKind == TKIND_COCLASS, (LPCWSTR)bstrName)));
-	}
+    std::wstring ret;
+    for (auto ch : str) {
+        ret.append(1, tolower(ch));
+    }
+    return ret;
 }
 
-int main(int argc, char **argv)
+void DumpFile(map<wstring, pair<bool, wstring>>& guids, const fs::path& filePath)
+{
+    wcout << L"Dump " << filePath << endl;
+
+    HRESULT hr = S_OK;
+    ITypeLibPtr spTypeLib;
+    VERIFHR(LoadTypeLibEx(filePath.wstring().c_str(), REGKIND_NONE, &spTypeLib));
+
+    UINT uiTypeInfoCount = spTypeLib->GetTypeInfoCount();
+    for (UINT i = 0; i < uiTypeInfoCount; ++i) {
+        TYPEKIND typeKind;
+        VERIFHR(spTypeLib->GetTypeInfoType(i, &typeKind));
+        if (typeKind != TKIND_COCLASS && typeKind != TKIND_DISPATCH && typeKind != TKIND_INTERFACE)
+            continue;
+
+        ITypeInfoPtr spTypeInfo;
+        VERIFHR(spTypeLib->GetTypeInfo(i, &spTypeInfo));
+
+        _bstr_t bstrName;
+        VERIFHR(spTypeInfo->GetDocumentation(MEMBERID_NIL, bstrName.GetAddress(), NULL, NULL, NULL));
+
+        TYPEATTR* pTypeAttr = nullptr;
+        VERIFHR(spTypeInfo->GetTypeAttr(&pTypeAttr));
+
+        wchar_t buffer[256];
+        if (StringFromGUID2(pTypeAttr->guid, buffer, 256) == 0)
+            continue;
+
+        wstring strGuid = (buffer + 1);
+        strGuid.pop_back();
+
+        spTypeInfo->ReleaseTypeAttr(pTypeAttr);
+        pTypeAttr = nullptr;
+
+        auto iter = guids.find(strGuid);
+        if (iter != guids.end())
+            continue;
+
+        guids.insert(make_pair(strGuid, make_pair(typeKind == TKIND_COCLASS, (LPCWSTR)bstrName)));
+    }
+}
+
+int main(int argc, char** argv)
 {
 
-	wcout.imbue(locale(""));
+    wcout.imbue(locale(""));
 
-	try
-	{
-		sys::path filePath;
+    try {
+        fs::path filePath;
 
-		if (argc <= 1)
-			filePath = sys::current_path();
-		else
-			filePath = argv[1];
-		if (!sys::exists(filePath))
-		{
-			wcout << L"File not exists! " << filePath << endl;
-			return -2;
-		}
+        if (argc <= 1)
+            filePath = fs::current_path();
+        else
+            filePath = argv[1];
+        if (!fs::exists(filePath)) {
+            wcout << L"File not exists! " << filePath << endl;
+            return -2;
+        }
 
-		map<sys::path, map<wstring, pair<bool, wstring>>> guids;
-		if (sys::is_directory(filePath))
-		{
-			sys::recursive_directory_iterator endIter;
-			for (sys::recursive_directory_iterator iter(filePath); iter != endIter; ++iter)
-			{
-				if (sys::is_directory(*iter))
-					continue;
+        map<fs::path, map<wstring, pair<bool, wstring>>> guids;
+        if (fs::is_directory(filePath)) {
+            fs::recursive_directory_iterator endIter;
+            for (fs::recursive_directory_iterator iter(filePath); iter != endIter; ++iter) {
+                if (fs::is_directory(*iter))
+                    continue;
 
-				auto ext = boost::to_lower_copy(iter->path().extension().wstring());
-				if (ext != L".dll" && ext != L".exe" && ext != L".idl" && ext != L".ocx" && ext != L".tlb")
-					continue;
+                auto ext = to_lower_copy(iter->path().extension().wstring());
+                if (ext != L".dll" && ext != L".exe" && ext != L".idl" && ext != L".ocx" && ext != L".tlb" && ext != L".dat")
+                    continue;
 
-				try
-				{
-					DumpFile(guids[*iter], *iter);
-				}
-				catch (const _com_error &e)
-				{
-					if (e.Error() != TYPE_E_CANTLOADLIBRARY)
-					{
-						wcout << L"FAILED : " << *iter << endl;
-						wcout << e.ErrorMessage() << endl;
-					}
-				}
-			}
-		}
-		else
-		{
-			try
-			{
-				DumpFile(guids[filePath], filePath);
-			}
-			catch (const _com_error &e)
-			{
-				wcout << L"FAILED : " << filePath << endl;
-				wcout << e.ErrorMessage() << endl;
-			}
-		}
+                try {
+                    DumpFile(guids[*iter], *iter);
+                } catch (const _com_error& e) {
+                    if (e.Error() != TYPE_E_CANTLOADLIBRARY) {
+                        wcout << L"FAILED : " << *iter << endl;
+                        wcout << e.ErrorMessage() << endl;
+                    }
+                }
+            }
+        } else {
+            try {
+                DumpFile(guids[filePath], filePath);
+            } catch (const _com_error& e) {
+                wcout << L"FAILED : " << filePath << endl;
+                wcout << e.ErrorMessage() << endl;
+            }
+        }
 
-		map<wstring, pair<bool, wstring>> uniqueGuids;
-		{
-			wofstream fout(L"guids.txt");
-			for (auto &item : guids)
-			{
-				fout << L"# " << item.first << endl;
-				for (auto &item2 : item.second)
-				{
-					fout << item2.first << L" " << item2.second.second << endl;
+        map<wstring, pair<bool, wstring>> uniqueGuids;
+        {
+            wofstream fout(L"guids.txt");
+            for (auto& item : guids) {
+                fout << L"# " << item.first << endl;
+                for (auto& item2 : item.second) {
+                    fout << item2.first << L" " << item2.second.second << endl;
 
-					if (uniqueGuids.count(item2.first) == 0)
-						uniqueGuids.insert(item2);
-				}
-			}
+                    if (uniqueGuids.count(item2.first) == 0)
+                        uniqueGuids.insert(item2);
+                }
+            }
 
-			fout << endl;
-		}
+            fout << endl;
+        }
 
-		{
-			wofstream foutClasses(INTERFACE_FILENAME);
-			wofstream foutInterfaces(CLASS_FILENAME);
+        {
+            wofstream foutClasses(CLASS_FILENAME);
+            wofstream foutInterfaces(INTERFACE_FILENAME);
 
-			for (auto &item : uniqueGuids)
-			{
-				if (item.second.first)
-				{
-					foutClasses << item.first << L" " << item.second.second << endl;
-				}
-				else
-				{
-					foutInterfaces << item.first << L" " << item.second.second << endl;
-				}
-			}
-		}
-	}
-	catch (const _com_error &e)
-	{
-		wcout << e.ErrorMessage() << endl;
-	}
+            for (auto& item : uniqueGuids) {
+                if (item.second.first) {
+                    foutClasses << item.first << L" " << item.second.second << endl;
+                } else {
+                    foutInterfaces << item.first << L" " << item.second.second << endl;
+                }
+            }
+        }
+    } catch (const _com_error& e) {
+        wcout << e.ErrorMessage() << endl;
+    }
 
-	cout << "finish!!!" << endl;
+    cout << "finish!!!" << endl;
 
-	return 0;
+    return 0;
 }
